b44164dc287ff9c6a24ca9f7b8e83b8a03e3cc51
jahid-sourav <info.jahidsourav@gmail.com>
2023-10-17 21:01:28 +0600

    Updated v 0.1

---
+++ assets/script/app.js
@@ -0,0 +1,10 @@
+const buttons = document.querySelectorAll('.button');
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        buttons.forEach(btn => {
+            btn.classList.remove('active');
+        });
+        button.classList.add('active');
+    });
+});
